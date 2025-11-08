9a79527e7f9ee31efee7c90ae654e4c4bbe55975
gorbayotte <mochonlucas@gmail.com>
2025-11-08 22:15:48 +0100

    delete coms

--- backend/internal/models/users/routes/users.routes.go
+++ backend/internal/models/users/routes/users.routes.go
@@ -1,8 +1,6 @@
 package usersRoutes
 
 import (
-	// "chesscom-copy/backend/internal/middlewares"
-
 	"chesscom-copy/backend/internal/common/middlewares"
 	usersController "chesscom-copy/backend/internal/models/users/controllers/users"
 
@@ -12,7 +10,6 @@ import (
 func RegisterUserRoutes(rg *gin.RouterGroup, uc *usersController.UserController) {
 	userGroup := rg.Group("/users")
 	{
-		// userGroup.GET("/", middlewares.JWTMiddleware(), uc.List)
 		userGroup.GET("/", middlewares.JWTMiddleware(), uc.List)
 		userGroup.GET("/:id", middlewares.JWTMiddleware(), uc.GetById)
 		userGroup.POST("/register", uc.Register)
